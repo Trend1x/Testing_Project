26082ea62c6863c47aed68b684e9751a8527b590
shen <shen@debian.com>
2019-10-17 17:15:57 -0500

    added ClassTransformVisitor

--- src/com/se4367/agents/Agent.java
+++ src/com/se4367/agents/Agent.java
@@ -16,9 +16,14 @@ public class Agent{
 	public static void premain(String agentArgs, Instrumentation inst){
 		inst.addTransformer(new ClassFileTransformer() {
 			@Override
-			public byte[]transform(ClassLoader classloader, String s, Class<?> someClass, ProtectionDomain protectionDomain, byte[] bytes)throws IllegalClassFormatException {
+			public byte[] transform(ClassLoader classloader, String s, Class<?> someClass, ProtectionDomain protectionDomain, byte[] bytes)throws IllegalClassFormatException {
 				
-				
+				if("class".contentEquals(s)) {
+					ClassReader cr = new ClassReader(bytes);
+					ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+					ClassTransformVisitor ca = new ClassTransformVisitor(cw);
+					cr.accept(ca, 0);
+				}
 				return null;
 			}
 		});
