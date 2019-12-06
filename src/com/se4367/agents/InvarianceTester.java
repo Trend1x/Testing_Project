0e269d4d614a1cf7acf5335fb674ec826f38b72d
shen <erik.fraim@gmail.com>
2019-12-06 15:42:43 -0600

    add InvarianceTester class

---
+++ src/com/se4367/agents/InvarianceTester.java
@@ -0,0 +1,45 @@
+package src.com.se4367.agents;
+
+import java.util.*;
+
+public class InvarianceTester {
+	
+	//Single Variables
+	//check to see if the variable is always a constant value
+	public static void constantValue(ArrayList<?> objs) {
+		
+	}
+	
+	//Uninitialized Value, has the variable been initialized or is it NULL
+	public static void uninitialized(ArrayList<?> objs) {
+		
+	}
+	
+	//is the variable a part of a small set
+	public static void smallSet(ArrayList<?> objs) {
+		
+	}
+	
+	
+	//Single Numeric Variables
+	//Does the variable always fall within a range between x and y
+	public static void inRange(ArrayList<?> objs) {
+		
+	}
+	
+	//Is the value always non-zero
+	public static void nonZero(ArrayList<?> objs) {
+		
+	}
+	
+	//Is the variable x == a mod(b) or is x != a mod(b)
+	public static void modulus(ArrayList<?> objs) {
+		
+	}
+	
+	
+	
+	
+	
+
+}
