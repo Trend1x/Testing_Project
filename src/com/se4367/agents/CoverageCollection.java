a008350424563a64258781c5f583e75901e89fc7
quanicus <phamquan90@gmail.com>
2019-12-06 14:28:37 -0600

    removed unnecessary commented code

--- src/com/se4367/agents/CoverageCollection.java
+++ src/com/se4367/agents/CoverageCollection.java
@@ -10,7 +10,6 @@ public class CoverageCollection {
 	public static String testCase;
 	
 	public static HashMap<String, HashMap<Integer, ArrayList<Object>>> localVar_Coverages;
-	//public static HashMap<Integer, ArrayList<Object>> localVar_Values;
 	public static String methodName;
 	
 	public static void addMethodLine(String className, Integer line){
@@ -52,37 +51,18 @@ public class CoverageCollection {
 		}
 	}
 	
-	public static void setMethodName(String mName) {
-		methodName = mName;
-		/*if (localVar_Coverages.get(methodName) == null)
-			localVar_Coverages.put(methodName, null);*/
-			
-	}
-	public static void addLocalVar(int index, int local) {
-		addVarCoverage(index, local);
-		//System.out.println("Local integer variable: " + local);
-	}
-	public static void addLocalVar(int index, long local) {
-		addVarCoverage(index, local);
-		//System.out.println("Local Long variable: " + local);
-	}
-	public static void addLocalVar(int index, double local) {
-		addVarCoverage(index, local);
-		//System.out.println("Local double variable: " + local);
-	}
-	public static void addLocalVar(int index, float local) {
-		addVarCoverage(index, local);
-		//System.out.println("Local float variable: " + local);
-	}
+	public static void setMethodName(String mName) { methodName = mName; }
+	
+	public static void addLocalVar(int index, int    local) { addVarCoverage(index, local); }	
+	public static void addLocalVar(int index, long   local) { addVarCoverage(index, local); }
+	public static void addLocalVar(int index, double local) { addVarCoverage(index, local); }
+	public static void addLocalVar(int index, float  local) { addVarCoverage(index, local); }
 	public static void addLocalVar(int index, Object local) {
 
 		if(local == null)
-			//System.out.println("Local object variable: NULL");
 			addVarCoverage(index, "null");
 		else {
 			addVarCoverage(index, "Object");
 		}			
 	}
-	
-
 }
